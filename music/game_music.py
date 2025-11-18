8cb6bc248634692d3dc5b8e4d7a2b24a7825b02d
williamhoman <wazevedo@student.highlands.edu>
2025-11-17 20:12:04 -0500

    Saving Draft Fourteen (11/17/25, 7:53pm)
    Draft

--- music/game_music.py
+++ music/game_music.py
@@ -3,7 +3,7 @@ from pygame import mixer
 
 def music():
     """This function runs the mechanics of the background music
-    of the game. Uses pygame to do so. """
+    of the game. Uses pygame to do so."""
     pygame.init()
     mixer.init()
     mixer.music.load("music/music.mp3")
