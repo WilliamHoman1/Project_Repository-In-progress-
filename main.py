d918e1d87faeef785311d1516606faf20488459f
williamhoman <wazevedo@student.highlands.edu>
2025-11-17 20:50:01 -0500

    Saving Draft Fourteen (11/17/25, 7:53pm)
    Draft

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import player
 from Scenes import scenes
 
 import game_engine
-"""Main function that starts the running of the game."""
+"""Main function that starts the running of the game. """
 if __name__ == "__main__":
     game_engine.start_game()
 
